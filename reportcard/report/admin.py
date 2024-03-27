815471013a37c252157a3b2f865edb5988cbb6b1
MANASI BIBEKAR <manasi.bibekar@adityabirlacapital.com>
2024-03-27 11:12:30 +0530

    created app | created models

---
+++ reportcard/report/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(StudentId)
+admin.site.register(Department)
+admin.site.register(Subject)
+admin.site.register(Student)
+admin.site.register(SubjectMarks)
---
+++ reportcard/report/seed.py
@@ -0,0 +1,49 @@
+from faker import Faker
+import random
+from .models import *
+
+fake = Faker()
+
+def seed_db(n=10):
+    try:
+        for _ in range(n):
+            department_objs = Department.objects.all()
+            random_index = random.randint(0, len(department_objs)-1)
+            student_id = f"STU-0{random.randint(100, 999)}"
+            department = department_objs[random_index]
+            student_name = fake.name()
+            student_email = fake.email()
+            student_age = random.randint(20, 30)
+            student_address = fake.address()
+
+            student_id_obj = StudentId.objects.create(student_id = student_id)
+
+            student_obj = Student.objects.create(
+                department = department,
+                student_id = student_id_obj,
+                student_name = student_name,
+                student_email = student_email, 
+                student_age = student_age,
+                student_address = student_address
+            )
+
+    except:
+        pass
+
+def create_subject_marks(n):
+    try:
+        student_objs = Student.objects.all()
+        for student in student_objs:
+            subjects = Subject.objects.all()
+            for subject in subjects:
+                SubjectMarks.objects.create(
+                    subject = subject,
+                    student = student,
+                    marks = random.randint(0, 100)
+                )
+    
+    except:
+        pass
+
+seed_db(100)
+
